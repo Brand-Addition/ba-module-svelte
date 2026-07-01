677aaf12b309664306fe6fa7186bb1563566fc7a
Sam Mear <sam.mear@brandaddition.com>
2026-07-01 16:07:37 +0100

    fix older messages.js exports

--- view/frontend/web/js/lib/messages.js
+++ view/frontend/web/js/lib/messages.js
@@ -1,7 +1,5 @@
 export {
     DEFAULT_MESSAGES_SELECTOR,
     STORE_MESSAGE_TYPES,
-    applyMessagePayload,
     dispatchStorefrontMessage,
-    updateMessageFragment,
 } from './messages/storefront.js';
