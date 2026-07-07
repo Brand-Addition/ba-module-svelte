91d444a529799b47b387440f4a829c309a33fc3e
Sam Mear <sam.mear@brandaddition.com>
2026-07-07 13:52:01 +0100

    quickfix

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -3,9 +3,6 @@ var config = {
         mixins: {
             'Magento_Theme/js/view/messages': {
                 'BA_Svelte/js/magento-mixin/messages': true
-            },
-            'Magento_Customer/js/customer-data': {
-                'BA_Svelte/js/magento-mixin/customer-sections': true
             }
         }
     }
